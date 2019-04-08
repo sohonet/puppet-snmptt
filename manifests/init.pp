c90f65e7f25894713c03ff9008445883b0e50407
Johan van den Dorpe <johan.vandendorpe@sohonet.com>
2019-04-08 15:15:57 +0200

    Add more configurable options

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,16 +12,19 @@
 #   handlers at http://snmptt.sourceforge.net/docs/snmptt.shtml
 #
 # [*strip_domain*]
-# Set to 1 to have SNMPTT strip the domain name from the host name passed to it.  For
+#   Set to 0 to enable the use of FQDN (Fully Qualified Domain Names).  If a host name is
+# passed to SNMPTT that contains a domain name, it will not be altered in any way by
+#   Set to 1 to have SNMPTT strip the domain name from the host name passed to it.  For
 # example, server01.domain.com would be changed to server01.
-# Set to 2 to have SNMPTT strip the domain name from the host name passed to it
+#   Set to 2 to have SNMPTT strip the domain name from the host name passed to it
 # based on the list of domains in strip_domain_list
+#   Default: 0
 #
 # [*strip_domain_list*]
 # Array. List of domain names that should be stripped when strip_domain is set to 2.
 #
 # [*enable_mysql*]
-# Boolean. Set to true to enable mysql support in SNMPTT. This will also create
+# Boolean.  Set to true to enable mysql support in SNMPTT. This will also create
 # a mysql database with the correct permissions and schema.
 #
 # [*mysql_host*]
@@ -53,6 +56,27 @@
 #   description text from the SNMPTT.CONF or MIB files.
 #   Default: false
 #
+# [*translate_log_trap_oid*]
+#   Configures how the OID of the received trap is handled when outputting to a
+#   log file / database
+#   Set to 0 to use the default of numerical OID
+#   Set to 1 to translate the trap OID to short text (symbolic form) (eg: linkUp)
+#   Set to 2 to translate the trap OID to short text with module name (eg: IF-MIB::linkUp)
+#   Set to 3 to translate the trap OID to long text (eg: iso...snmpTraps.linkUp)
+#   Set to 4 to translate the trap OID to long text with module name (eg:
+#     IF-MIB::iso...snmpTraps.linkUp)
+#   Default: 0
+#
+# [*mibs_environment*]
+#   Allows you to set the MIBS environment variable used by SNMPTT
+#   Default: ''
+#
+# [*date_time_format*]
+#   The date time format in strftime() format for the date/time when logging
+#   to standard output, snmptt log files (log_file) and the unknown log file
+#   (unknown_trap_log_file).  Defaults to localtime().
+#   Default: ''
+#
 # === Examples
 #
 # See tests folder.
@@ -66,7 +90,7 @@ class snmptt (
   Boolean $service_enable           = true,
   Boolean $multiple_event           = true,
   Boolean $dns_enable               = false,
-  Boolean $strip_domain             = false,
+  Integer $strip_domain             = 0,
   Array $strip_domain_list          = [],
   Boolean $log_enable               = true,
   Boolean $log_system_enable        = false,
@@ -81,11 +105,13 @@ class snmptt (
   Array $trap_files                 = ['/etc/snmp/snmptt.conf'],
   Boolean $net_snmp_perl_enable     = false,
   Boolean $description_mode         = false,
+  Integer $translate_log_trap_oid   = 0,
+  String $mibs_environment          = '',
+  String $date_time_format          = '',
 ) {
 
   $real_multiple_event          = bool2num($multiple_event)
   $real_dns_enable              = bool2num($dns_enable)
-  $real_strip_domain            = bool2num($strip_domain)
   $real_log_enable              = bool2num($log_enable)
   $real_log_system_enable       = bool2num($log_system_enable)
   $real_unknown_trap_log_enable = bool2num($unknown_trap_log_enable)
